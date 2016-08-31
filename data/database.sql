fe444bd896ba2388ccda660ad31a599261d25f7f
iticus <ticus.ionut@gmail.com>
2016-08-31 23:12:41 +0300

    added .gitignore, removed unused db index

--- data/database.sql
+++ data/database.sql
@@ -9,5 +9,5 @@ CREATE TABLE readings(
     UNIQUE (day, county_id)
 );
 
-CREATE INDEX ON readings(day);
+--CREATE INDEX ON readings(day); not needed due to the UNIQUE constraint
 CREATE INDEX ON readings(county_id);
